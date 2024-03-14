1e0c215bd6c5f1cd77c73e12555528a9964ca65a
kreikeland <kasper.eikeland@gmail.com>
2024-03-14 19:26:26 +0100

    calculate in meters

--- magnetic.py
+++ magnetic.py
@@ -5,10 +5,11 @@ import matplotlib.cm as cm
 import argparse
 
 # constants
+R_e = 6.37e6 # radius of Earth, m
 alpha = (23+11) * np.pi / 180 # angle of inclination of Earth's magnetic field, 11deg + 23deg
-M = 30.4e-6 * np.array([[np.cos(alpha), 0, -np.sin(alpha)],
+M = 8e15 * np.array([[np.cos(alpha), 0, -np.sin(alpha)],
                         [0,1,0],
-                        [np.sin(alpha), 0, np.cos(alpha)]]) @ np.array([0,0,1]) # Earth's magnetic dipole moment, µT * R_e**3, rotated
+                        [np.sin(alpha), 0, np.cos(alpha)]]) @ np.array([0,0,1]) # Earth's magnetic dipole moment, T x m**3, rotated
 
 def B(x,y,z, m):
     """
@@ -24,14 +25,14 @@ def B(x,y,z, m):
 
 if __name__ == '__main__':
     # Plot xz-plane
-    x, z = np.linspace(-6, 6, 100), np.linspace(-6, 6, 100)
+    x, z = np.linspace(-6*R_e, 6*R_e, 100), np.linspace(-6*R_e, 6*R_e, 100)
     X, Z = np.meshgrid(x, z)
     Y = 0
     Bx, By, Bz = B(X, Y, Z, M)
 
 
     fig, ax = plt.subplots()
-    ax.streamplot(X, Z, Bx, Bz, color='k',linewidth=1,
+    ax.streamplot(X/R_e, Z/R_e, Bx, Bz, color='k',linewidth=1,
                 density=2, arrowstyle='->', arrowsize=1.5)
     ax.add_patch(Circle((0, 0), 1, color='k', zorder=100))
     # p = ax.contourf(X, Z, By, cmap=cm.jet, levels=0) # contour for By, pos/neg
@@ -44,13 +45,13 @@ if __name__ == '__main__':
     plt.show()
 
     # Plot xy-plane
-    x, y = np.linspace(-6, 6, 100), np.linspace(-6, 6, 100)
+    x, y = np.linspace(-6*R_e, 6*R_e, 100), np.linspace(-6*R_e, 6*R_e, 100)
     X, Y = np.meshgrid(x, y)
     Z = 0
     Bx, By, Bz = B(X, Y, Z, M)
 
     fig, ax = plt.subplots()
-    ax.streamplot(X, Y, Bx, By, color='k',linewidth=1,
+    ax.streamplot(X/R_e, Y/R_e, Bx, By, color='k',linewidth=1,
                 density=2, arrowstyle='->', arrowsize=1.5)
     ax.add_patch(Circle((0, 0), 1, color='k', zorder=100))
     # p = ax.contourf(X, Y, Bz, cmap=cm.jet, levels=0) # contour map for Bz, pos/neg
